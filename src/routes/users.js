78241b627d0c846afe3e9b39cca94b26c124460a
kersh2000 <bradleykershaw333@gmail.com>
2022-11-10 15:00:57 +0000

    Added GET Request for all users.

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,4 +5,9 @@ const { User } = require('../models/User');
 
 usersRouter.use(express.json());
 
+usersRouter.get('/', async (req, res) => {
+  const users = await User.findAll();
+  res.status(200).send(users);
+});
+
 module.exports = usersRouter;
