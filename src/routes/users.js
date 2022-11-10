3e1ce18003772087df3ecbcc5d277e9d95bf7218
kersh2000 <bradleykershaw333@gmail.com>
2022-11-10 15:29:30 +0000

    Added GET requests to pull specific user + shows.

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,27 @@ usersRouter.get('/', async (req, res) => {
   res.status(200).send(users);
 });
 
+usersRouter.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("Could not find user.");
+  }
+});
+
+usersRouter.get('/:id/shows', async (req, res) => {
+  const user = await User.findByPk(req.params.id);
+  const shows = await user.getShows();
+  if (shows) {
+    res.status(200).send(shows);
+  } else {
+    res.status(404).send("Could not find shows.");
+  }
+})
+
+usersRouter.put('/:id/shows/:showId', async (req, res) => {
+  
+});
+
 module.exports = usersRouter;
