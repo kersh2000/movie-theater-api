64f4baae2a46f42d745fff1f0c894beac7eb1bc3
kersh2000 <bradleykershaw333@gmail.com>
2022-11-10 15:02:33 +0000

    Seeded database before listening + added routers.

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const { usersRouter, showsRouter } = require('./routes');
-const db = require('./db/db');
+const seed = require('./db/seed');
 
 app.use(express.json());
 
+app.use('/users', usersRouter);
+app.use('/shows', showsRouter);
+
 //Allows app to listen on given port, so it can be queried
 app.listen(port, async () => {
-  await db.sync();
+  await seed();
   console.log(`Listening on port ${port}...`)
 });
