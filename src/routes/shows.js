86fe6a374ed10c9693c51b7285a03e931fa751a0
kersh2000 <bradleykershaw333@gmail.com>
2022-11-11 09:31:46 +0000

    Added middleware.

--- src/routes/shows.js
+++ src/routes/shows.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const showsRouter = express.Router();
 const { Show } = require('../models/Show');
+const validShow = require('../middleware/validShow');
 
 showsRouter.use(express.json());
 
@@ -10,13 +11,8 @@ showsRouter.get('/', async (req, res) => {
   res.status(200).send(shows);
 });
 
-showsRouter.get('/:id', async (req, res) => {
-  const show = await Show.findByPk(req.params.id);
-  if (show) {
-    res.status(200).send(show);
-  } else {
-    res.status(404).send("Could not find show.");
-  }
+showsRouter.get('/:id', validShow, async (req, res) => {
+  res.status(200).send(req.show);
 });
 
 showsRouter.get('/genres/:genre', async (req, res) => {
@@ -32,15 +28,45 @@ showsRouter.get('/genres/:genre', async (req, res) => {
   }
 });
 
-showsRouter.put('/:id/watched', async (req, res) => {
+showsRouter.put('/:id/watched',
+body('rating')
+.not().isEmpty()
+.withMessage('Rating must be provided.')
+.custom(value => !/\s/.test(value))
+.withMessage('Rating must not conatin any spaces.'),
+validShow, 
+async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(401).send(errors);
+  }
+  await req.show.update({
+    rating: req.body.rating
+  });
+  res.status(200).send(`Show ${req.params.id} successfully updated.`);
+});
+
+showsRouter.put('/:id/updates/:custom'
+, 
+validShow, async (req, res) => {
+  const arr = ['cancelled', 'on-going'];
+  if (req.params.custom === 'true') {
+    await req.show.update({
+      status: (arr[1 - arr.indexOf(req.show.status)])
+    });
+    res.status(200).send(`Successfully changed status of show ${req.params.id}`);
+  } else {
+
+  }
+});
+
+showsRouter.delete('/:id', async (req, res) => {
   try {
     const show = await Show.findByPk(req.params.id);
-    await show.update({
-      rating: req.body.rating
-    });
-    res.status(200).send(`Show ${req.params.id} successfully updated.`);
+    await show.destroy();
+    res.status(200).send(`Show ${req.params.id} successfully deleted.`);
   } catch (error) {
-    res.status(404).send(`Could not update rating to show ${req.params.id}.`)
+    res.status(404).send(`Failed to delete show ${req.params.id}.`);
   }
 });
 
